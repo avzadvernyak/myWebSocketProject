9245b2134dd48cf9aaa1de8cbcd4e794421ed97c
Kampukter <a.v.zadvernyak@gmail.com>
2019-03-09 15:44:24 +0300

    9/3/2019

--- app/src/main/java/kampukter/myWebSocketProject/MainFragment.kt
+++ app/src/main/java/kampukter/myWebSocketProject/MainFragment.kt
@@ -12,7 +12,8 @@ import kotlinx.android.synthetic.main.main_fragment.*
 import okhttp3.*
 import java.util.concurrent.TimeUnit
 
-val temperature =  MutableLiveData<String>()
+val temperature  =  MutableLiveData<String>()
+val logProcessWS =  MutableLiveData<String>()
 class MainFragment : Fragment() {
 
     override fun onCreateView(
@@ -36,10 +37,16 @@ class MainFragment : Fragment() {
         // this provide to make 'Open ws connection'
         client.newWebSocket(request, wsListener).run { Log.d("blablabla", "Start") }
 
-        temperature.observe(this, Observer { temp-> temperatureTextView.text = temp })
+        temperature.observe(this, Observer { temp-> firstSensorValueTextView.text = temp })
+        logProcessWS.observe(this, Observer { msg-> logTextView.text = msg })
 
         startButton.setOnClickListener {
+            logTextView.text = "Press Start Button"
             Log.d("blablabla", "Press Start Button")
+        }
+        stopButton.setOnClickListener {
+            logTextView.text = "Press Stop Button"
+            Log.d("blablabla", "Press Stop Button")
         }
     }
 
@@ -48,10 +55,12 @@ class MainFragment : Fragment() {
 private class EchoWebSocketListener : WebSocketListener() {
     override fun onOpen(webSocket: WebSocket, response: Response) {
         super.onOpen(webSocket, response)
+        logProcessWS.postValue( "Press Stop Button" )
         Log.v("blablabla", "Open WebSocket")
     }
     override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
         super.onFailure(webSocket, t, response)
+        logProcessWS.postValue( """Failure t - ${t.toString()}""" )
         Log.v("blablabla", """Failure t - ${t.toString()}""")
         Log.v("blablabla", """Failure response - ${response.toString()}""")
     }
@@ -59,11 +68,14 @@ private class EchoWebSocketListener : WebSocketListener() {
         super.onMessage(webSocket, text)
         Log.v("blablabla", "Receiving : $text")
         temperature.postValue(text)
+        logProcessWS.postValue( "Receiving : $text" )
     }
 
     override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
         super.onClosing(webSocket, code, reason)
         Log.v("blablabla", "Closing : $code / $reason")
+
+        logProcessWS.postValue( "Closing : $code / $reason" )
         webSocket!!.close(NORMAL_CLOSURE_STATUS, null)
     }
 
