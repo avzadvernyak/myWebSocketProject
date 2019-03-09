14ef7ac292ac3b17646fada9bef50f0174854029
Kampukter <a.v.zadvernyak@gmail.com>
2019-03-09 20:36:09 +0300

    9/3/2019

--- app/src/main/java/kampukter/myWebSocketProject/MainFragment.kt
+++ app/src/main/java/kampukter/myWebSocketProject/MainFragment.kt
@@ -9,14 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.main_fragment.*
-import okhttp3.*
-import java.util.concurrent.TimeUnit
 import android.content.Intent
-
-
 
 val temperature  =  MutableLiveData<String>()
 val logProcessWS =  MutableLiveData<String>()
+
 class MainFragment : Fragment() {
 
     override fun onCreateView(
@@ -28,17 +25,6 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        val client = OkHttpClient.Builder()
-            .readTimeout(10, TimeUnit.SECONDS)
-            .build()
-        val request = Request.Builder()
-            .url("ws://192.168.0.82:81")
-            .build()
-        val wsListener = EchoWebSocketListener()
-
-        // this provide to make 'Open ws connection'
-        client.newWebSocket(request, wsListener).run { Log.d("blablabla", "Start") }
 
         temperature.observe(this, Observer { temp-> firstSensorValueTextView.text = temp })
         logProcessWS.observe(this, Observer { msg-> logTextView.text = msg })
@@ -56,36 +42,4 @@ class MainFragment : Fragment() {
         }
     }
 
-}
-
-private class EchoWebSocketListener : WebSocketListener() {
-    override fun onOpen(webSocket: WebSocket, response: Response) {
-        super.onOpen(webSocket, response)
-        logProcessWS.postValue( "Press Stop Button" )
-        Log.v("blablabla", "Open WebSocket")
-    }
-    override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
-        super.onFailure(webSocket, t, response)
-        logProcessWS.postValue( """Failure t - ${t.toString()}""" )
-        Log.v("blablabla", """Failure t - ${t.toString()}""")
-        Log.v("blablabla", """Failure response - ${response.toString()}""")
-    }
-    override fun onMessage(webSocket: WebSocket?, text: String?) {
-        super.onMessage(webSocket, text)
-        Log.v("blablabla", "Receiving : $text")
-        temperature.postValue(text)
-        logProcessWS.postValue( "Receiving : $text" )
-    }
-
-    override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
-        super.onClosing(webSocket, code, reason)
-        Log.v("blablabla", "Closing : $code / $reason")
-
-        logProcessWS.postValue( "Closing : $code / $reason" )
-        webSocket!!.close(NORMAL_CLOSURE_STATUS, null)
-    }
-
-    companion object {
-        private val NORMAL_CLOSURE_STATUS = 1000
-    }
 }
--- app/src/main/java/kampukter/myWebSocketProject/WebSocketService.kt
+++ app/src/main/java/kampukter/myWebSocketProject/WebSocketService.kt
@@ -4,11 +4,32 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import okhttp3.*
+import java.util.concurrent.TimeUnit
 
 class WebSocketService : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    lateinit var client: OkHttpClient
+
+    override fun onBind(intent: Intent): IBinder? {
+
+        Log.d("blablabla", "onBind service")
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        client = OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build()
+        val request = Request.Builder()
+            .url("ws://192.168.0.82:81")
+            .build()
+        val wsListener = EchoWebSocketListener()
+
+        // this provide to make 'Open ws connection'
+        client.newWebSocket(request, wsListener).run { Log.d("blablabla", "Start") }
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
@@ -23,6 +44,40 @@ class WebSocketService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
+
         Log.d("blablabla", "Stop service")
+    }
+
+    private class EchoWebSocketListener : WebSocketListener() {
+
+        override fun onOpen(webSocket: WebSocket, response: Response) {
+            super.onOpen(webSocket, response)
+            logProcessWS.postValue( "Press Stop Button" )
+            Log.v("blablabla", "Open WebSocket")
+        }
+        override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
+            super.onFailure(webSocket, t, response)
+            logProcessWS.postValue( """Failure t - ${t.toString()}""" )
+            Log.v("blablabla", """Failure t - ${t.toString()}""")
+            Log.v("blablabla", """Failure response - ${response.toString()}""")
+        }
+        override fun onMessage(webSocket: WebSocket?, text: String?) {
+            super.onMessage(webSocket, text)
+            Log.v("blablabla", "Receiving : $text")
+            temperature.postValue(text)
+            logProcessWS.postValue( "Receiving : $text" )
+        }
+
+        override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
+            super.onClosing(webSocket, code, reason)
+            Log.v("blablabla", "Closing : $code / $reason")
+
+            logProcessWS.postValue( "Closing : $code / $reason" )
+            webSocket!!.close(NORMAL_CLOSURE_STATUS, null)
+        }
+
+        companion object {
+            private val NORMAL_CLOSURE_STATUS = 1000
+        }
     }
 }
